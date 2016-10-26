d22420611687cd5ded1d479feefc126203127f51
Lionell Briones <llenoil@gmail.com>
2016-10-26 09:03:29 +0800

    ngSwitch

--- src/app/stocks.component.ts
+++ src/app/stocks.component.ts
@@ -3,25 +3,10 @@ import { StockService } from './stock.service';
 
 @Component({
     selector: 'stocks',
-    template: `<h2>Stocks</h2>
-    {{title}}
-
-    <ul>
-        <li *ngFor="let stock of stocks">
-            {{stock}}
-        </li>
-    </ul>
-
-    <hr>
-
-    <ul *ngIf="stockMarkets.length > 0">
-        <li *ngFor="let stockMarket of stockMarkets">
-            {{stockMarket}}
-        </li>
-    </ul>
-    `
+    templateUrl: './stocks.component.html'
 })
 export class StocksComponent{
+    market = 'NYSE';
     title = 'List of stocks: ';
     stocks;
 
